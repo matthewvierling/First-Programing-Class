02b66d697898a2001cb20ca4383745ecac478414
matthewvierling <matthewvierling@ymail.com>
2017-02-18 14:48:48 -0500

    committing first program

---
+++ Vierling_Section2_Lab1_c.c
@@ -0,0 +1,31 @@
+//First Program--lab1: picture
+//Matthew Vielring
+//aug/27/2015
+//section 2
+
+#include<stdio.h>
+
+int main()
+{	
+	printf("\tDr. Yampolskiy\n\n");
+	printf("\t     +++\n");
+	printf("\t   +++++++\n");
+	printf("\t  +++@+@+++\n");
+	printf("\t  ++\\___/++\n");
+	printf("\t   +++++++\n");
+	printf("\t     +++\n");
+	printf("\t  $$$$$$$$$$\n");
+	printf("\t $$ $$$$$$ $$\n");
+	printf("\t $$ $$$$$$ $$\n");
+	printf("\t $$ $$$$$$ $$\n");
+	printf("\t ++ [][][] ++\n");
+	printf("\t    %%%%%%%%%%%%\n");
+	printf("\t    %%%%%%%%%%%%\n");
+	printf("\t    %%%%  %%%%\n");
+	printf("\t    %%%%  %%%%\n");
+	printf("\t    %%%%  %%%%\n");
+	printf("\t___###__###___\n");
+	
+	return 0;
+	
+}
